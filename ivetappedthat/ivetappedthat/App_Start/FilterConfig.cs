9ab538494d9d93b5d1d7bc0e25c3b3d2a43b0b91
Jacob Wolniewicz <jacob@wolniewicz.com>
2013-11-30 11:58:55 -0700

    Initializing Project

---
+++ ivetappedthat/ivetappedthat/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ivetappedthat
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ivetappedthat/ivetappedthat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ivetappedthat.MvcApplication" Language="C#" %>
