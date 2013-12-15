4b432a37f069efe7472c80b6c8dd94e68e7e58f0
Jacob Wolniewicz <jacob@wolniewicz.com>
2013-12-14 17:02:18 -0700

    Refactoring

--- IveTappedThat/mainPage.py
+++ IveTappedThat/mainPage.py
@@ -62,35 +62,6 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('pages/index.html')
         self.response.write(template.render(template_values))
 
-"""
-class AddBeer(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each Greeting
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
-        beerlist_name = self.request.get('beerlist_name',
-                                          BEER_LIST_NAME)
-        beer = Beer(parent=beerlist_key(beerlist_name))
-
-        if users.get_current_user():
-            beer.author = users.get_current_user()
-
-        beer.brewery = self.request.get('beerBrewery')
-        beer.name = self.request.get('beerName')
-        beer.abv = self.request.get('beerABV').replace("%", "");
-
-
-
-        if users.get_current_user():
-            beer.put()    
-
-        query_params = {'beerlist_name': beerlist_name}
-        self.redirect('/?' + urllib.urlencode(query_params))    
-"""
-
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-   # ('/addBeer', AddBeer)
+    ('/', MainPage)
 ], debug=True)
